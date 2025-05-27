7a44695609bd63e2fe048ae25c9f80ee5402b3d3
Rafly Hanggaraksa <ajarafly422@gmail.com>
2025-05-26 19:15:59 -0600

    nit: refactor flag mapping

--- tracer/IOTracer.py
+++ tracer/IOTracer.py
@@ -14,6 +14,102 @@ import threading
 
 THRESHOLD_NS = 1000000
 
+class FlagMapper:
+    def __init__(self):
+        # https://github.com/analogdevicesinc/linux/blob/main/include/linux/blk_types.h#L370
+        self.flag_fs_map = {
+            0o00000000: "O_RDONLY",
+            0o00000001: "O_WRONLY", 
+            0o00000002: "O_RDWR",
+            0o00000100: "O_CREAT",
+            0o00000200: "O_EXCL",
+            0o00000400: "O_NOCTTY",
+            0o00001000: "O_TRUNC",
+            0o00002000: "O_APPEND",
+            0o00004000: "O_NONBLOCK",
+            0o00010000: "O_DSYNC",
+            0o00040000: "O_DIRECT",
+            0o00100000: "O_LARGEFILE",
+            0o00200000: "O_DIRECTORY",
+            0o00400000: "O_NOFOLLOW",
+            0o01000000: "O_NOATIME",
+            0o02000000: "O_CLOEXEC",
+            0o04010000: "O_SYNC",
+            0o010000000: "O_PATH",
+            0o020200000: "O_TMPFILE"
+        }
+
+        # https://elixir.bootlin.com/linux/v6.14.6/source/include/linux/blk_types.h#L312
+        self.op_block_types = {
+            0: "REQ_OP_READ",
+            1: "REQ_OP_WRITE",
+            2: "REQ_OP_FLUSH",
+            3: "REQ_OP_DISCARD",
+            5: "REQ_OP_SECURE_ERASE",
+            7: "REQ_OP_ZONE_APPEND",
+            9: "REQ_OP_WRITE_ZEROES",
+            10: "REQ_OP_ZONE_OPEN",
+            11: "REQ_OP_ZONE_CLOSE",
+            12: "REQ_OP_ZONE_FINISH",
+            13: "REQ_OP_ZONE_RESET",
+            15: "REQ_OP_ZONE_RESET_ALL",
+            34: "REQ_OP_DRV_IN",
+            35: "REQ_OP_DRV_OUT",
+            36: "REQ_OP_LAST"
+        }
+
+        self.op_fs_types = {
+            1: "READ",
+            2: "WRITE",
+            3: "OPEN",
+            4: "CLOSE",
+            5: "FSYNC"
+        }
+
+    def format_block_operation(self, flags):
+        result = [self.op_block_types.get(flags, f"[UNKNOWN_OP({flags})]")]
+        return "|".join(result) if result else "NO_FLAGS"
+
+    def format_fs_flags(self, flags):
+        # handle access mode specially
+        access_mode = flags & 0o3  # mask with 0b11 (3 in decimal)
+        access_str = None
+        if access_mode == 0o0:
+            access_str = "O_RDONLY"
+        elif access_mode == 0o1:
+            access_str = "O_WRONLY"
+        elif access_mode == 0o2:
+            access_str = "O_RDWR"
+            
+        result = []
+        if access_str:
+            result.append(access_str)
+            
+        # check for other flags
+        # skip the access mode flags we already handled
+        for flag, name in self.flag_fs_map.items():
+            if name in ["O_RDONLY", "O_WRONLY", "O_RDWR"]:
+                continue
+                
+            # special handling for O_SYNC because it includes O_DSYNC
+            if name == "O_SYNC" and flags & 0o04010000:
+                result.append(name)
+                continue
+                
+            # specil handling for O_TMPFILE coz it includes O_DIRECTORY
+            if name == "O_TMPFILE" and (flags & 0o020200000) == 0o020200000:
+                result.append(name)
+                # remove O_DIRECTORY if it's already in the list since it's part of O_TMPFILE
+                if "O_DIRECTORY" in result:
+                    result.remove("O_DIRECTORY")
+                continue
+                
+            # handle all other regular flags
+            if name not in ["O_SYNC", "O_TMPFILE"] and flags & flag:
+                result.append(name)
+        
+        return "|".join(result) if result else "NO_FLAGS"
+
 class WriteManager:
     def __init__(self, output_dir: str | None):
         self.output_dir = output_dir if output_dir else f"./result/vfs_trace_analysis_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
@@ -60,7 +156,6 @@ class WriteManager:
         json.dump(log_output, self._block_json_handle, indent=2)
 
     def write_to_disk(self, json_events: list, json_block_events: list, log_output: str, log_block_output: str):
-        # TODO: Execute this on another thread
         t1 = threading.Thread(target=self.write_log_vfs, args=(log_output,))
         t2 = threading.Thread(target=self.write_log_block, args=(log_block_output,))
         t3 = threading.Thread(target=self.write_log_vfs_json, args=(json_events,))
@@ -100,6 +195,8 @@ class IOTracer:
         self.bpf = None
 
         self.writer = WriteManager(output_dir)
+        self.flag_mapper = FlagMapper()
+
         self.log_output = ''
         self.log_block_output = ''
 
@@ -179,23 +276,16 @@ class IOTracer:
         global event_count, args, running, json_events, last_event_time
         
         event = self.b["events"].event(data)
-        # print(f"{event.ts} FILESYSTEM LAYER -> PID:{event.pid}")
-        op_name = self.op_names.get(event.op, "[unknown]")
+        op_name = self.flag_mapper.op_fs_types.get(event.op, "[unknown]")
         
         try:
             filename = event.filename.decode()
         except UnicodeDecodeError:
             filename = "[decode_error]"
         
-        flags_str = self._format_flags(event.flags)
+        flags_str = self.flag_mapper.format_fs_flags(event.flags)
         
         raw_time = event.ts
-        # if raw_time - last_event_time < self.debouncing_duration:
-        #     return
-        last_event_time = raw_time
-
-        # ts = raw_time / 1000000000 
-        # timestamp = datetime.fromtimestamp(ts).strftime('%H:%M:%S.%f')[:-3]
         timestamp = raw_time
         
         try:
@@ -209,8 +299,8 @@ class IOTracer:
         
 
 
-        # if self.verbose:
-        #     print(output)
+        if self.verbose:
+            print(output)
         
         # write to file
         self.log_output += output + "\n"
@@ -237,18 +327,14 @@ class IOTracer:
         global event_count, args, running, json_block_events, last_event_time
         
         event = self.b["bl_events"].event(data)
-        # op_name = self.op_names.get(event.op, "UNKNOWN")
         
-        # print(f"{event.ts} BLOCK LAYER -> PID:{event.pid}")
-        # print("%d %d %d" % (event.pid, event.ts, event.sector))
         timestamp = event.ts
         pid = event.pid
         comm = event.comm.decode('utf-8', errors='replace')
         sector = event.sector
         nr_sectors = event.nr_sectors
-        ops_str = self._format_flags(event.op, is_block=True)
+        ops_str = self.flag_mapper.format_block_operation(event.op)
         output = f"{timestamp} {pid} {comm.replace(' ','_')} {sector} {nr_sectors} {ops_str}"
-         # write to file
 
         self.log_block_output += output + "\n"
         
@@ -263,91 +349,6 @@ class IOTracer:
         }
         json_block_events.append(json_event)
 
-    def _format_flags(self, flags, is_block=False):
-        if not is_block:
-            # https://github.com/analogdevicesinc/linux/blob/main/include/linux/blk_types.h#L370
-            flag_map = {
-                0o00000000: "O_RDONLY",
-                0o00000001: "O_WRONLY", 
-                0o00000002: "O_RDWR",
-                0o00000100: "O_CREAT",
-                0o00000200: "O_EXCL",
-                0o00000400: "O_NOCTTY",
-                0o00001000: "O_TRUNC",
-                0o00002000: "O_APPEND",
-                0o00004000: "O_NONBLOCK",
-                0o00010000: "O_DSYNC",
-                0o00040000: "O_DIRECT",
-                0o00100000: "O_LARGEFILE",
-                0o00200000: "O_DIRECTORY",
-                0o00400000: "O_NOFOLLOW",
-                0o01000000: "O_NOATIME",
-                0o02000000: "O_CLOEXEC",
-                0o04010000: "O_SYNC",
-                0o010000000: "O_PATH",
-                0o020200000: "O_TMPFILE"
-            }
-        
-            # handle access mode specially
-            access_mode = flags & 0o3  # mask with 0b11 (3 in decimal)
-            access_str = None
-            if access_mode == 0o0:
-                access_str = "O_RDONLY"
-            elif access_mode == 0o1:
-                access_str = "O_WRONLY"
-            elif access_mode == 0o2:
-                access_str = "O_RDWR"
-                
-            result = []
-            if access_str:
-                result.append(access_str)
-                
-            # check for other flags
-            # skip the access mode flags we already handled
-            for flag, name in flag_map.items():
-                if name in ["O_RDONLY", "O_WRONLY", "O_RDWR"]:
-                    continue
-                    
-                # special handling for O_SYNC because it includes O_DSYNC
-                if name == "O_SYNC" and flags & 0o04010000:
-                    result.append(name)
-                    continue
-                    
-                # specil handling for O_TMPFILE coz it includes O_DIRECTORY
-                if name == "O_TMPFILE" and (flags & 0o020200000) == 0o020200000:
-                    result.append(name)
-                    # remove O_DIRECTORY if it's already in the list since it's part of O_TMPFILE
-                    if "O_DIRECTORY" in result:
-                        result.remove("O_DIRECTORY")
-                    continue
-                    
-                # handle all other regular flags
-                if name not in ["O_SYNC", "O_TMPFILE"] and flags & flag:
-                    result.append(name)
-            
-            return "|".join(result) if result else "NO_FLAGS"
-        else:
-            # https://elixir.bootlin.com/linux/v6.14.6/source/include/linux/blk_types.h#L312
-            op_types = {
-                0: "REQ_OP_READ",
-                1: "REQ_OP_WRITE",
-                2: "REQ_OP_FLUSH",
-                3: "REQ_OP_DISCARD",
-                5: "REQ_OP_SECURE_ERASE",
-                7: "REQ_OP_ZONE_APPEND",
-                9: "REQ_OP_WRITE_ZEROES",
-                10: "REQ_OP_ZONE_OPEN",
-                11: "REQ_OP_ZONE_CLOSE",
-                12: "REQ_OP_ZONE_FINISH",
-                13: "REQ_OP_ZONE_RESET",
-                15: "REQ_OP_ZONE_RESET_ALL",
-                34: "REQ_OP_DRV_IN",
-                35: "REQ_OP_DRV_OUT",
-                36: "REQ_OP_LAST"
-            }
-
-            result = [op_types.get(flags, f"[UNKNOWN_OP({flags})]")]
-            return "|".join(result) if result else "NO_FLAGS"
 
     def _flush(self):
         global json_events, json_block_events, flushing
