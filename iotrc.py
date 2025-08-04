8aa9fc53cc8e474df7c0ce71a74095d01b06cdcf
Rafly <ajarafly422@gmail.com>
2025-08-04 02:37:26 -0400

    fix: update default flush threshold to 50000

--- iotrc.py
+++ iotrc.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--page-cnt', type=int, default=8, help='Number of pages for perf buffer (default 8)')
     parser.add_argument('-v', '--verbose', type=bool, default=False, help='Print verbose output')
     parser.add_argument('-d', '--duration', type=int, help='Duration to run the tracer in seconds. Default is NULL (run indefinitely)')
-    parser.add_argument('-f', '--flush_threshold', type=int, default=10000, help='Buffered flush threshold in array length (default 10000)')
+    parser.add_argument('-f', '--flush_threshold', type=int, default=50000, help='Buffered flush threshold in array length (default 50000)')
     parser.add_argument('-s', '--split_threshold', type=int, default=3600 * 24, help='Split threshold in seconds (default 1 day)')
 
     parse_args = parser.parse_args()
