3361c5aa71826a9d21a7cff7c7e463522ffab3a4
Rafly Hanggaraksa <ajarafly422@gmail.com>
2025-12-08 00:43:09 -0700

    minor

--- iotrc.py
+++ iotrc.py
@@ -7,11 +7,10 @@ from src.tracer.IOTracer import IOTracer
 if __name__ == "__main__":
     app_version = "vRelease"
     parser = argparse.ArgumentParser(description='Trace IO syscalls')
-    parser.add_argument('-o', '--output', type=str, default="./result", help='Output Directory for logging, must be new!')
+    parser.add_argument('-o', '--output', type=str, default="./result", help='Output Directory for logging')
     parser.add_argument('-v', '--verbose', type=bool, default=False, help='Print verbose output')
     parser.add_argument('-a', '--anonimize', action='store_true', help='Enable anonymization of process and file names')
     parser.add_argument('-au', '--auto-upload', action='store_true', help='Enable automatic upload of logs')
-    parser.add_argument('-s', '--server-mode', action='store_true', help='Optimized for higher throughput in server environments')
     parser.add_argument('--dev', action='store_true', help='Developer mode with extra logs and checks')
 
     parse_args = parser.parse_args()
