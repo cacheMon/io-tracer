a7e217da993477449af551b24cdd7f31599441b4
Rafly Hanggaraksa <ajarafly422@gmail.com>
2025-12-09 18:14:09 -0700

    set upload as default

--- iotrc.py
+++ iotrc.py
@@ -10,11 +10,18 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', type=str, default="./result", help='Output Directory for logging')
     parser.add_argument('-v', '--verbose', type=bool, default=False, help='Print verbose output')
     parser.add_argument('-a', '--anonimize', action='store_true', help='Enable anonymization of process and file names')
-    parser.add_argument('-au', '--auto-upload', action='store_true', help='Enable automatic upload of logs')
+    parser.add_argument('-l', '--local-only', action='store_true', help='Disable automatic upload of logs, save the trace locally')
     parser.add_argument('--dev', action='store_true', help='Developer mode with extra logs and checks')
 
     parse_args = parser.parse_args()
     output_dir = parse_args.output.strip()
+    
+    is_upload = not parse_args.local_only
+    
+    if is_upload:
+        print("[*] Automatic upload enabled, logs will be uploaded during tracing.")
+    else:
+        print("[*] Local only mode enabled, automatic upload disabled.")
 
     tracer = IOTracer(
         output_dir=output_dir,
@@ -22,7 +29,7 @@ if __name__ == "__main__":
         page_cnt=8,
         verbose=parse_args.verbose,
         anonymous=parse_args.anonimize,
-        automatic_upload=parse_args.auto_upload,
+        automatic_upload=is_upload,
         developer_mode=parse_args.dev,
         version=app_version,
     )
